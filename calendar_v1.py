30933351959883ab459a2dc417f952febdeeb845
Fluxonic <dev.contact.zone@gmail.com>
2025-06-02 17:37:44 +0900

    Add initial version of calendar (manual print)

---
+++ calendar_v1.py
@@ -0,0 +1,24 @@
+print('2030')
+print('June')
+print('Mon Tue Wed Thu Fri Sat Sun')
+print()
+print('2030')
+print('July')
+print('Mon Tue Wed Thu Fri Sat Sun')
+print()
+print('2030')
+print('August')
+print('Mon Tue Wed Thu Fri Sat Sun')
+
+# Output:
+# 2030
+# June
+# Mon Tue Wed Thu Fri Sat Sun
+
+# 2030
+# July
+# Mon Tue Wed Thu Fri Sat Sun
+
+# 2030
+# August
+# Mon Tue Wed Thu Fri Sat Sun
